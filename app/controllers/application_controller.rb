5c40207520986795f7b0a52fcd1aefad399546cc
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-10-28 08:02:05 +0100

    added params to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name location])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
