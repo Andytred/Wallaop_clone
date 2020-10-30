d16c3cc62a2950141edd3f5afacf8ac3d473a267
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-10-30 19:03:37 +0100

    set up bullet gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.rails_logger = true
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
