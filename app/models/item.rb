9b0d7b9c64638117beef780b3580911b19a3194d
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-11-02 17:37:33 +0100

    scoped availability through active record associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
     belongs_to :user
+    scope :available, -> { where(available: true) }
+
     has_many :transactions, dependent :destroy
+
+    CATEGORIES = ['Books','Clothes', 'Decoration', 'Hobbies', 'Home', 'Kids', 'Music', 'Services', 'Sports', 'Vehicles', 'Other'].freeze
+
 end
