4d93f893b19c70c7a12daa7e673d2679ff8d521b
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-11-02 17:33:32 +0100

    migrated availability for items

---
+++ db/migrate/20201102161529_add_availability_to_items.rb
@@ -0,0 +1,5 @@
+class AddAvailabilityToItems < ActiveRecord::Migration[6.0]
+  def change
+    add_column :items, :available, :boolean, default: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_080445) do
+ActiveRecord::Schema.define(version: 2020_11_02_161529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_10_28_080445) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "category"
+    t.boolean "available", default: true
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
